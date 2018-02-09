a458e3366e61b0442da8bc3840b4fbd3f9c33610
Bill Groves <billyggroves@gmail.com>
2018-02-09 10:24:51 -0500

    pushing to heroku

--- application.py
+++ application.py
@@ -259,7 +259,9 @@ def login():
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows.user_id
+        session.user_id = rows.user_id
+
+        print("session.user_id...... " + str(session.get("user_id")))
 
         # Redirect user to home page
         return redirect("/")
@@ -336,7 +338,7 @@ def register():
         #                   username=request.form.get("username"))
 
         # Remember which user has logged in
-        session["user_id"] = rows.user_id
+        session.user_id = rows.user_id
         print("REGISTER.............. " + str(rows.user_id))
 
         # Redirect user to home page
