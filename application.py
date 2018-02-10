db64444cafe17817f0bbb21669223daeaafcd16c
Bill Groves <billyggroves@gmail.com>
2018-02-10 17:53:52 -0500

    pushing to heroku

--- application.py
+++ application.py
@@ -105,7 +105,7 @@ if app.config["DEBUG"]:
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
